856d5bf06d392b5c69208e7ae7cc287b66a0c15c
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-03-16 19:17:17 +0530

    Added component that displays the list of problems

---
+++ app/components/problems/ProblemList.jsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { FaRegCheckCircle } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+
+const ProblemList = ({ id, sno=1, title, difficulty, type }) => {
+	const router = useRouter();
+	const handleClick = () => {
+		console.log("Problem clicked", id);
+		router.push(`/problems/${id}`);
+	};
+	return (
+		<div
+			className={ "w-1/2 cursor-pointer p-2 flex flex-row text-md text-white font-semibold bg-gray-800 border-gray-500 border-y-[1px]" }
+			onClick={handleClick}
+		>
+			<div className={ "w-1/6 mx-2 flex" }>
+				<div className={ "px-1 flex items-center text-green-700" }>
+					<FaRegCheckCircle/>
+				</div>
+				<div className={ "px-2" }>
+					{ sno }
+				</div>
+			</div>
+
+			<div className={ "w-3/6 px-6" }>
+				{ title }
+			</div>
+			<div className={ "w-1/6 px-6" }>
+				{ type }
+			</div>
+			<div className={ "w-1/6 px-2 text-center" }>
+				{ difficulty }
+			</div>
+		</div>
+	);
+};
+
+export default ProblemList;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,41 @@
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+import getProblems from "@/app/actions/getProblems";
+import ProblemList from "@/app/components/problems/ProblemList";
+import { FaRegCheckCircle } from "react-icons/fa";
 
+export default async function Home() {
+	const problems = await getProblems();
+
+	return (
+		<main className="">
+			<div className={"w-full flex flex-col items-center"}>
+				<div className={ "w-1/2 p-2 flex flex-row text-lg text-white font-semibold bg-gray-800 border-gray-500 border-y-[1px]" }>
+					<div className={ "w-1/6 mx-2 flex" }>
+						<div className={ "px-2" }>
+							S.No
+						</div>
+					</div>
+
+					<div className={ "w-3/6 px-6" }>
+						Problem
+					</div>
+					<div className={ "w-1/6 px-6" }>
+						Topic
+					</div>
+					<div className={ "w-1/6 px-2 text-center" }>
+						Difficulty
+					</div>
+				</div>
+
+				{ problems.map((problem) => (
+					<ProblemList
+						key={ problem.id }
+						id={ problem.id }
+						title={ problem.title }
+						difficulty={ problem.difficulty }
+						type={ problem.type }
+					/>
+				)) }
+			</div>
 		</main>
 	);
 }
