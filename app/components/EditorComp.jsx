a08d87742d71ad7b86c9fea7a5c91c6a152b7860
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-04-10 15:09:03 +0530

    Allowed dropdown menu to be passed as props to Editor

--- app/components/EditorComp.jsx
+++ app/components/EditorComp.jsx
@@ -1,13 +1,38 @@
 'use client'
 import Editor from '@monaco-editor/react';
 
-const EditorComp = () => {
+const EditorComp = ({ selected }) => {
+	const options = {
+		autoIndent: 'full',
+		contextmenu: true,
+		fontFamily: 'monospace',
+		fontSize: 13,
+		lineHeight: 24,
+		hideCursorInOverviewRuler: true,
+		matchBrackets: 'always',
+		minimap: {
+			enabled: true,
+		},
+		scrollbar: {
+			horizontalSliderSize: 4,
+			verticalSliderSize: 18,
+		},
+		selectOnLineNumbers: true,
+		roundedSelection: false,
+		readOnly: false,
+		cursorStyle: 'line',
+		automaticLayout: true,
+	};
+
+	console.log(selected);
+
 	return (
 		<div>
 			<Editor
 				height="90vh"
 				// width="100vh"
 				defaultLanguage="javascript"
+				language={ selected }
 				defaultValue="// Write your code here"
 				theme="vs-dark"
 			/>
--- app/problems/[problemID]/ProblemClient.jsx
+++ app/problems/[problemID]/ProblemClient.jsx
@@ -1,17 +1,22 @@
 'use client';
 
-import React from "react";
+import React, { useState } from "react";
 import "@/app/resizable.css";
 import { ResizableBox } from "react-resizable";
 import ProblemDescription from "@/app/components/problems/ProblemDescription";
 import EditorComp from "@/app/components/EditorComp";
 import Dropdown from "@/app/components/Dropdown";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ProblemClient = ({ problem }) => {
 	const languages = ["c", "cpp","csharp","java","javascript","lua","python","r","ruby","typescript"]
 
+	const [selected, setSelected] = useState(localStorage.getItem("selectedItem") || items[0]);
+
+
 	return (
-		<div className={ "flex w-full" }>
+		<div className={ "flex w-full h-full" }>
 			<ResizableBox
 				className="custom-box box border-r-2 border-black"
 				width={ 800 }
@@ -30,21 +35,19 @@ const ProblemClient = ({ problem }) => {
 			<div className={"mt-4 px-4 w-[50vw] flex-grow"} >
 				<div className={"flex justify-between items-center px-4 py-1 bg-neutral-700/50 rounded-t-xl"}>
 					<div className={"text-white font-medium flex items-center"}>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-							<path fillRule="evenodd"
-								  d="M14.447 3.026a.75.75 0 0 1 .527.921l-4.5 16.5a.75.75 0 0 1-1.448-.394l4.5-16.5a.75.75 0 0 1 .921-.527ZM16.72 6.22a.75.75 0 0 1 1.06 0l5.25 5.25a.75.75 0 0 1 0 1.06l-5.25 5.25a.75.75 0 1 1-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 0 1 0-1.06Zm-9.44 0a.75.75 0 0 1 0 1.06L2.56 12l4.72 4.72a.75.75 0 0 1-1.06 1.06L.97 12.53a.75.75 0 0 1 0-1.06l5.25-5.25a.75.75 0 0 1 1.06 0Z"
-								  clipRule="evenodd" />
-						</svg>
 
+						<FontAwesomeIcon icon={faCode} />
 						<h1 className={"ml-2"}>Code Editor</h1>
 
 					</div>
 					<Dropdown
 						items={ languages }
+						onSelect = { setSelected }
+						selected = { selected }
 					/>
 				</div>
 
-				<EditorComp />
+				<EditorComp selected = {selected}/>
 			</div>
 		</div>
 
