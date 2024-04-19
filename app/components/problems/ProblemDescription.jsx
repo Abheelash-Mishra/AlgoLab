143d0c7a37a6e98032409f6c83e2f82e34e38135
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-04-19 21:45:21 +0530

    Modified problem statement styles

--- app/components/problems/ProblemDescription.jsx
+++ app/components/problems/ProblemDescription.jsx
@@ -1,20 +1,28 @@
 'use client'
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ProblemDescription = ({ problem }) => {
+	// const [height, setHeight] = useState(0);
+	//
+	// useEffect(() => {
+	// 	setHeight(window.innerHeight - 110);
+	// }, []);
+	//
+	// console.log(height);
+
 	return (
-		<div className={ "text-left text-white m-4 px-6 py-4 h-[91vh] bg-neutral-700/50 rounded-xl" }>
+		<div className={ `text-left text-white m-4 px-6 py-4 bg-neutral-700/50 rounded-xl h-[860px]` }>
 			<div className={ "flex justify-between items-center" }>
 				<h1 className={ "text-2xl font-bold" }>
 					{ problem.title }
 				</h1>
 
 				<div className={ "flex" }>
-					<h3 className={ "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-primary/70 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-default" }>
+					<h3 className={ "text-gray-900 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-primary/70 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-default" }>
 						{ problem.type }
 					</h3>
-					<h3 className={ "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-primary/70 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-default" }>
+					<h3 className={ "text-gray-900 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-primary/70 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-default" }>
 						{ problem.difficulty }
 					</h3>
 				</div>
@@ -32,25 +40,29 @@ const ProblemDescription = ({ problem }) => {
 			<br />
 			<br />
 
-			<p className={ "my-4" }>
-				<strong>Sample Input<br /></strong>
+			<div className={ "my-4 border-2 border-gray-800 rounded-lg bg-gray-700 px-4 py-2" }>
+				<div className={"pb-4"}>
+					<strong>Sample Input<br/></strong>
+				</div>
 				{ problem.sampleInput.split('\n').map((line, index) => (
 					<React.Fragment key={ index }>
 						{ line }
 						<br />
 					</React.Fragment>
 				)) }
-			</p>
-			<p className={ "my-4" }>
-				<strong>Sample Output<br /></strong>
+			</div>
+
+			<div className={ "my-4 border-2 border-gray-800 rounded-lg bg-gray-700 px-4 py-2" }>
+				<div className={ "pb-4" }>
+					<strong>Sample Output<br /></strong>
+				</div>
 				{ problem.sampleOutput.split('\n').map((line, index) => (
 					<React.Fragment key={ index }>
 						{ line }
 						<br />
 					</React.Fragment>
 				)) }
-			</p>
-
+			</div>
 		</div>
 	);
 };
